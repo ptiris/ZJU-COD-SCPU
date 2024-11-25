fb54965fac52577d744fa11432be96221f14258e
Ptiris <3230106269@zju.edu.cn>
2024-11-25 19:26:19 +0800

    update: add debug ports to support VGA test

--- src/CSRRegs.v
+++ src/CSRRegs.v
@@ -8,41 +8,87 @@ module CSRRegs(
     input [31:0]mepc_bypasss_in,
     input [31:0]mscause_bypass_in,
     input [31:0]mtval_bypass_in,
-    input [31:0]mtvec_bypass_in,
     input [31:0]mstatus_bypass_in,
     
-    output [31:0] rdata,             // 读出 CSR 寄存器的数据
+    output reg [31:0]rdata,             // 读出 CSR 寄存器的数据
     output [31:0]mepc_bypasss_out,
     output [31:0]mscause_bypass_out,
     output [31:0]mtval_bypass_out,
     output [31:0]mtvec_bypass_out,
     output [31:0]mstatus_bypass_out
 );
-    reg [31:0] res[4095:0];
+    // reg [31:0] res[4095:0];
+    // integer i;
+    // always @(posedge clk or posedge rst) begin
+    //     if(rst)begin
+    //         for (i = 0;i < 4096;i = i+1) 
+    //             res[i]<=0;
+    //     end
+    //     else begin
+    //         if(expt_int && (csr_wsc_mode == 2'b01))begin
+    //             res[12'h341] <= mepc_bypasss_in;
+    //             res[12'h342] <= mscause_bypass_in;
+    //             res[12'h343] <= mtval_bypass_in;
+    //             res[12'h300] <= mstatus_bypass_in;
+    //         end
+    //         else if(waddr && csr_w)
+    //             res[waddr] <= wdata;
+    //         else res[waddr] <= res[waddr];
+    //     end
+    // end
+
+    // assign  rdata = res[raddr];
+    // assign  mepc_bypasss_out   =  res[833];
+    // assign  mscause_bypass_out =  res[834];
+    // assign  mtval_bypass_out   =  res[835];
+    // assign  mtvec_bypass_out   =  res[773];
+    // assign  mstatus_bypass_out =  res[768];
+
+    localparam mepc_addr    = 12'h341;
+    localparam mscause_addr = 12'h342;
+    localparam mtval_addr   = 12'h343;
+    localparam mtvec_addr   = 12'd773;
+    localparam mstatus_addr = 12'h300;
+
+    reg [31:0] res[4:0];
     integer i;
     always @(posedge clk or posedge rst) begin
         if(rst)begin
-            for (i = 0;i < 4096;i = i+1) 
-                res[i]<=0;
+            for(i=0;i<5;i=i+1)
+                res[i] <= 0;
         end
         else begin
             if(expt_int && (csr_wsc_mode == 2'b01))begin
-                res[12'h341] <= mepc_bypasss_in;
-                res[12'h342] <= mscause_bypass_in;
-                res[12'h343] <= mtval_bypass_in;
-                res[12'h305] <= mtvec_bypass_in;
-                res[12'h300] <= mstatus_bypass_in;
+                res[0] <= mepc_bypasss_in;
+                res[1] <= mscause_bypass_in;
+                res[2] <= mtval_bypass_in;
+                res[3] <= mstatus_bypass_in;
             end
             else if(waddr && csr_w)
-                res[waddr] <= wdata;
-            else res[waddr] <= res[waddr];
+            begin
+                case (waddr)
+                    mepc_addr:     res[0] <= wdata;
+                    mscause_addr:  res[1] <= wdata;
+                    mtval_addr:    res[2] <= wdata;
+                    mstatus_addr:  res[3] <= wdata;
+                    mtvec_addr:    res[4] <= wdata;
+                endcase
+            end
+            case (raddr)
+                mepc_addr:     rdata <= res[0];
+                mscause_addr:  rdata <= res[1];
+                mtval_addr:    rdata <= res[2];
+                mstatus_addr:  rdata <= res[3];
+                mtvec_addr:    rdata <= res[4]; 
+                default:       rdata <= 0;
+            endcase
         end
     end
 
-    assign  rdata = res[raddr];
-    assign  mepc_bypasss_out   =  res[833];
-    assign  mscause_bypass_out =  res[834];
-    assign  mtval_bypass_out   =  res[835];
-    assign  mtvec_bypass_out   =  res[773];
-    assign  mstatus_bypass_out =  res[768];
+    assign  mepc_bypasss_out   =  res[0];
+    assign  mscause_bypass_out =  res[1];
+    assign  mtval_bypass_out   =  res[2];
+    assign  mstatus_bypass_out =  res[3];
+    assign  mtvec_bypass_out   =  res[4];
+        
 endmodule
--- src/DataPath.v
+++ src/DataPath.v
@@ -29,7 +29,54 @@ module DataPath(
     output [31:0]    ALU_out,
     output [31:0]    Data_out,
     output [3:0]     Save_base,
-    output reg [31:0]    PC_out
+    output reg [31:0]    PC_out,
+
+    output [31:0] Reg00, 
+    output [31:0] Reg01, 
+    output [31:0] Reg02, 
+    output [31:0] Reg03, 
+    output [31:0] Reg04, 
+    output [31:0] Reg05, 
+    output [31:0] Reg06, 
+    output [31:0] Reg07, 
+    output [31:0] Reg08, 
+    output [31:0] Reg09, 
+    output [31:0] Reg10, 
+    output [31:0] Reg11, 
+    output [31:0] Reg12, 
+    output [31:0] Reg13, 
+    output [31:0] Reg14, 
+    output [31:0] Reg15, 
+    output [31:0] Reg16, 
+    output [31:0] Reg17, 
+    output [31:0] Reg18, 
+    output [31:0] Reg19, 
+    output [31:0] Reg20, 
+    output [31:0] Reg21, 
+    output [31:0] Reg22, 
+    output [31:0] Reg23, 
+    output [31:0] Reg24, 
+    output [31:0] Reg25, 
+    output [31:0] Reg26, 
+    output [31:0] Reg27, 
+    output [31:0] Reg28, 
+    output [31:0] Reg29, 
+    output [31:0] Reg30, 
+    output [31:0] Reg31,
+    output [31:0] Data_in_field,
+
+    output [31:0]Rs1_data,
+    output [31:0] Rs2_data,
+    output [31:0]Rd_data,
+    output [4:0]Rs1_addr,
+    output [4:0]Rs2_addr,
+    output [4:0]Rd_addr,
+
+    output [31:0]mepc_bypasss_out,
+    output [31:0]mscause_bypass_out,
+    output [31:0]mtval_bypass_out,
+    output [31:0]mtvec_bypass_out,
+    output [31:0]mstatus_bypass_out
 );
 
     reg [31:0]PC_next;
@@ -47,9 +94,12 @@ module DataPath(
         .Imm_out(Imm_out)
     );
 
-    wire [31:0]Rd_data,ALU_B,ALU_out;
+    wire [31:0]ALU_B;
     wire [31:0]PC_4,PC_BJ,PC_RD;//Branch or Jump Results for PC.
     wire [31:0]Rs1_data,Rs2_data;
+    assign Rs1_addr = inst_in[19:15];
+    assign Rs2_addr = inst_in[24:20];
+    assign Rd_addr  = inst_in[11:7];
 
     Regs REG_U2(
         .clk(clk),
@@ -60,14 +110,45 @@ module DataPath(
         .Rs1_data(Rs1_data), 
         .Rs2_data(Rs2_data),
         .Wt_data(Rd_data), 
-        .RegWrite(RegWrite)
+        .RegWrite(RegWrite),
+        .Reg00(Reg00), 
+        .Reg01(Reg01), 
+        .Reg02(Reg02), 
+        .Reg03(Reg03), 
+        .Reg04(Reg04), 
+        .Reg05(Reg05), 
+        .Reg06(Reg06), 
+        .Reg07(Reg07), 
+        .Reg08(Reg08), 
+        .Reg09(Reg09), 
+        .Reg10(Reg10), 
+        .Reg11(Reg11), 
+        .Reg12(Reg12), 
+        .Reg13(Reg13), 
+        .Reg14(Reg14), 
+        .Reg15(Reg15), 
+        .Reg16(Reg16), 
+        .Reg17(Reg17), 
+        .Reg18(Reg18), 
+        .Reg19(Reg19), 
+        .Reg20(Reg20), 
+        .Reg21(Reg21), 
+        .Reg22(Reg22), 
+        .Reg23(Reg23), 
+        .Reg24(Reg24), 
+        .Reg25(Reg25), 
+        .Reg26(Reg26), 
+        .Reg27(Reg27), 
+        .Reg28(Reg28), 
+        .Reg29(Reg29), 
+        .Reg30(Reg30), 
+        .Reg31(Reg31)
     );
     /*  
         PC_BJ = P1 + P2 
         P1 = R1_data or PC
         P2 = imm
     */
-    wire [31:0]Data_in_field;
     wire [7:0]Data_bias = (ALU_out[1:0] << 3);
     wire [1:0]Byte_bias = ALU_out[1:0];
     Data_inGen Data_in_U3(
@@ -93,8 +174,9 @@ module DataPath(
     assign csr_wdata = (csr_opctrl == 2'b00)?csr_opnum:
     (csr_opctrl == 2'b01)?csr_opnum|csr_rdata:(~csr_opnum)&csr_rdata;
     
-    reg [31:0]mepc_bypasss_in,mscause_bypass_in,mtval_bypass_in,mtvec_bypass_in,mstatus_bypass_in;
+    reg [31:0]mepc_bypasss_in,mscause_bypass_in,mtval_bypass_in,mstatus_bypass_in;
     wire [31:0]mepc_bypasss_out,mscause_bypass_out,mtval_bypass_out,mtvec_bypass_out,mstatus_bypass_out;
+    reg [1:0]csr_wsc_mode;
     CSRRegs CSR_U5(
         .clk(clk),
         .rst(rst),
@@ -108,7 +190,6 @@ module DataPath(
         .mepc_bypasss_in(mepc_bypasss_in),
         .mscause_bypass_in(mscause_bypass_in),
         .mtval_bypass_in(mtval_bypass_in),
-        .mtvec_bypass_in(mtvec_bypass_in),
         .mstatus_bypass_in(mstatus_bypass_in),
         
         .mepc_bypasss_out(mepc_bypasss_out),
@@ -117,17 +198,20 @@ module DataPath(
         .mtvec_bypass_out(mtvec_bypass_out),
         .mstatus_bypass_out(mstatus_bypass_out)
         );
-    reg [1:0]csr_wsc_mode;
     always @(*) begin
         if(mstatus_bypass_out[3] && expt_int)begin
-            csr_wsc_mode = 2'b01;
+            csr_wsc_mode      = 2'b01;
             mstatus_bypass_in = {mstatus_bypass_out[31:4],1'b0,mscause_bypass_out[2:0]};    //set trap enable
             mscause_bypass_in = {ecall|IO_break,28'b0,IO_break,ecall,ill_inst};             //set cause for trap
             mepc_bypasss_in   = PC_out;
             mtval_bypass_in   = inst_in;
         end 
         else begin
-            csr_wsc_mode = 2'b00;
+            csr_wsc_mode      = 2'b00;
+            mstatus_bypass_in = mstatus_bypass_out;    //set trap enable
+            mscause_bypass_in = mscause_bypass_out;             //set cause for trap
+            mepc_bypasss_in   = mepc_bypasss_out;
+            mtval_bypass_in   = mtval_bypass_out;
         end
     end
 
